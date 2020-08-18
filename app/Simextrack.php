70eb168b83d239bea52445cc010bd9bc5d9aa947
Granite Bagas <granitebagas28@gmail.com>
2020-08-18 15:26:20 +0700

    Add api for simextrack download tracking

---
+++ app/Simextrack.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Simextrack extends Model
+{
+    protected $fillable = ['ip', 'version'];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Simextrack;
 use App\Traits\Helpers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -80,6 +82,18 @@ Route::group(['namespace' => 'Api\v1', 'prefix' => 'v1'], function () {
 
     Route::get('blog', 'BlogController@index');
     Route::get('blog/{id}', 'BlogController@show');
+
+    Route::get('simextrack/v1', function (Request $request) {
+        $check_ip = Simextrack::where('ip', $request->ip())->first();
+        if (!$check_ip) {
+            Simextrack::create([
+                'ip' => $request->ip(),
+                'version' => 'v1'
+            ]);
+        }
+
+        return response()->download(public_path('images/simextrack/v1/simextrack-prod-v1.apk'));
+    });
 
     Route::any('{path}', function () {
         return Helpers::apiResponse(false, 'Not Found', [], 404);
