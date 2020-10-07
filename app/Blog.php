dfa3b9e0257d1b89677cce0f8286f8c8e5125510
Granite Bagas <granitebagas28@gmail.com>
2020-10-07 16:10:29 +0700

    :sparkles: Add soft delete to blogs table

--- app/Blog.php
+++ app/Blog.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Cache;
 
 class Blog extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'blogs';
     protected $guarded = ['created_at', 'updated_at'];
 
