cdf62ca1e334d8e5e4f3eb3749e2b0ef9a13151d
Granite Bagas <granitebagas28@gmail.com>
2020-10-31 00:24:11 +0700

    :ambulance: Fix email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,9 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
+            @if (trim($slot) === 'GBPS')
             <img src="https://s3.ap-southeast-1.amazonaws.com/portfolio-granitebps.com/galeries/1600601933_gbps.png"
-                class="logo" alt="Laravel Logo">
+                class="logo" alt="GBPS Logo">
             @else
             {{ $slot }}
             @endif
