787e8a4691e213b234f74eeff1a63963d2f20635
Granite <granitebagas28@gmail.com>
2021-04-03 21:51:47 +0700

    :construction: Update dashboard api

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -12,15 +12,15 @@ class DashboardController extends Controller
     public function dashboard(Request $request)
     {
         $data = Log::all();
-        $dates = $data->pluck('date')->unique();
+        $dates = $data->pluck('date')->unique()->sortDesc();
 
         $logs = [];
         foreach ($dates as $date) {
-            $log = $data->where('date', $date);
+            $log = $data->where('date', $date)->sortByDesc('count');
             array_push($logs, [
                 'date' => $date,
+                'data' => $log->flatten(),
                 'total' => $log->sum('count'),
-                'data' => $log,
             ]);
         }
 
