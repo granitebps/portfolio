d5f1b699488d694e4388be4a6bfd6ee0dc9d16f7
Granite <granitebagas28@gmail.com>
2021-04-04 13:07:45 +0700

    :bug: Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,27 @@ set -e
 
 echo "Deploying application ..."
 
-# Enter maintenance mode
-(php artisan down) || true
-    # Update codebase
-    git fetch origin master
-    git reset --hard origin/master
+# Update codebase
+git fetch origin master
+git reset --hard origin/master
 
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+# Install dependencies based on lock file
+composer install --no-interaction --prefer-dist --optimize-autoloader
 
-    # Migrate database
-    php artisan migrate --force
+# Migrate database
+php artisan migrate --force
 
-    # Note: If you're using queue workers, this is the place to restart them.
-    # ...
+# Note: If you're using queue workers, this is the place to restart them.
+# ...
 
-    # Run test
-    php artisan test
+# Run test
+php artisan test
 
-    # Clear cache
-    php artisan optimize
-    php artisan cache:clear
+# Clear cache
+php artisan optimize
+php artisan cache:clear
 
-    # Reload PHP to update opcache
-    echo "" | sudo -S service php7.4-fpm reload
-# Exit maintenance mode
-php artisan up
+# Reload PHP to update opcache
+echo "" | sudo -S service php7.4-fpm reload
 
 echo "Application deployed!"
