09a64102fe0f7f457f115c82c826ce087234e372
Granite Bagas <granitebagas28@gmail.com>
2021-09-12 14:17:09 +0700

    :fire: Remove log api

--- app/Http/Controllers/Api/v1/DashboardController.php
+++ app/Http/Controllers/Api/v1/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Models\ViewModels\Log;
 use Illuminate\Http\Request;
 use App\Traits\Helpers;
 
@@ -11,18 +10,7 @@ class DashboardController extends Controller
 {
     public function dashboard(Request $request)
     {
-        $data = Log::all();
-        $dates = $data->pluck('date')->unique()->sortDesc();
-
         $logs = [];
-        foreach ($dates as $date) {
-            $log = $data->where('date', $date)->sortByDesc('count');
-            array_push($logs, [
-                'date' => $date,
-                'data' => $log->flatten(),
-                'total' => $log->sum('count'),
-            ]);
-        }
 
         return Helpers::apiResponse(true, 'Logs Retrieved', $logs);
     }
