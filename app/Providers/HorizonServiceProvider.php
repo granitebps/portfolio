34a7fc3190d365b5c1c3823e0ebc506aa39fb27c
Granite Bagas <granitebagas28@gmail.com>
2021-10-01 23:56:28 +0700

    :ambulance: Fix horizon auth

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -25,18 +25,15 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     }
 
     /**
-     * Register the Horizon gate.
-     *
-     * This gate determines who can access Horizon in non-local environments.
+     * Overload authorization method from \Laravel\Horizon\HorizonApplicationServiceProvider
+     * to allow access to Horizon without having a logged in user.
      *
      * @return void
      */
-    protected function gate()
+    protected function authorization()
     {
-        Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+        Horizon::auth(function ($request) {
+            return true;
         });
     }
 }
