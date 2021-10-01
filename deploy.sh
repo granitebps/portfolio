1f6574545731b22d3b1d46270db0b2afddcabc9b
Granite Bagas <granitebagas28@gmail.com>
2021-10-01 22:42:39 +0700

    :construction: Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,27 @@ set -e
 
 echo "Deploying application ..."
 
+# Go To Folder
+cd /var/www/portfolio
+
 # Update codebase
-git fetch origin master
-git reset --hard origin/master
+git pull origin master
 
 # Install dependencies based on lock file
-composer install --no-interaction --prefer-dist --optimize-autoloader
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 # Migrate database
 php artisan migrate --force
 
-# Note: If you're using queue workers, this is the place to restart them.
-# ...
-
 # Clear cache
-php artisan optimize
+php artisan optimize:clear
 php artisan cache:clear
+php artisan optimize
 
 # Reload PHP to update opcache
-echo "" | sudo -S service php7.4-fpm reload
+echo "" | sudo -S service php8.0-fpm reload
+
+# Restart Horizon
+cd /etc/supervisor/ && supervisorctl restart horizon
 
 echo "Application deployed!"
