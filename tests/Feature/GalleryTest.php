9f83860085b24ca674e5a83d6aa9a5497e6e1077
Granite Bagas <granitebagas28@gmail.com>
2021-11-27 18:50:00 +0700

    :white_check_mark: Update gallery test to pest

---
+++ tests/Feature/GalleryTest.php
@@ -0,0 +1,88 @@
+<?php
+
+use App\Models\Gallery;
+use App\Models\Profile;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+use function Pest\Laravel\delete;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+it('can get list of gallery when authenticated', function (Profile $profile) {
+    $token = $profile->user->createToken(config('app.name'))->plainTextToken;
+
+    get(
+        uri: route('gallery.index'),
+    )->assertStatus(401);
+
+    get(
+        uri: route('gallery.index'),
+        headers: [
+            'Authorization' => "Bearer $token",
+        ]
+    )->assertStatus(200)->assertJson(
+        fn (AssertableJson $json) =>
+        $json->has('data', 1)
+            ->etc()
+    );
+})->with('profile', 'gallery');
+
+it('can store a gallery when authenticated', function (Profile $profile) {
+    $token = $profile->user->createToken(config('app.name'))->plainTextToken;
+    $gallery = Gallery::factory()->make();
+    $file = UploadedFile::fake()->image('gallery.jpg');
+
+    $this->assertDatabaseCount('galeries', 0);
+
+    post(
+        uri: route('gallery.store'),
+    )->assertStatus(401);
+
+    $response = post(
+        uri: route('gallery.store'),
+        data: [
+            'name' => $gallery->name,
+            'ext' => $gallery->ext,
+            'size' => $gallery->size,
+            'file' => $file
+        ],
+        headers: [
+            'Authorization' => "Bearer $token",
+        ],
+    )->assertStatus(200)->json();
+
+    expect(Gallery::find($response['data']['id']))->toBeTruthy();
+})->with('profile');
+
+it('can delete a gallery when authenticated', function (Profile $profile, Gallery $gallery) {
+    $token = $profile->user->createToken(config('app.name'))->plainTextToken;
+
+    expect(Gallery::where('id', $gallery->id)->count())->toEqual(1);
+
+    delete(
+        uri: route('gallery.destroy', [
+            'gallery' => 0
+        ]),
+    )->assertStatus(401);
+
+    delete(
+        uri: route('gallery.destroy', [
+            'gallery' => 0
+        ]),
+        headers: [
+            'Authorization' => "Bearer $token",
+        ]
+    )->assertStatus(404);
+
+    delete(
+        uri: route('gallery.destroy', [
+            'gallery' => $gallery->id
+        ]),
+        headers: [
+            'Authorization' => "Bearer $token",
+        ]
+    )->assertStatus(200);
+
+    expect(Gallery::where('id', $gallery->id)->count())->toEqual(0);
+})->with('profile', 'gallery');
