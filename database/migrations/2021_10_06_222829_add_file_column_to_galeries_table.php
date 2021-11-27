061a4a91e76c6487b1fa2b3909ed869f4b203ce0
Granite Bagas <granitebagas28@gmail.com>
2021-11-27 21:45:24 +0700

    :green_heart: Fix CI

--- database/migrations/2021_10_06_222829_add_file_column_to_galeries_table.php
+++ database/migrations/2021_10_06_222829_add_file_column_to_galeries_table.php
@@ -14,7 +14,7 @@ class AddFileColumnToGaleriesTable extends Migration
     public function up()
     {
         Schema::table('galeries', function (Blueprint $table) {
-            $table->string('file');
+            $table->string('file')->nullable();
         });
     }
 
