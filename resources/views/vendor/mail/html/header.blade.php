c08334520fe92eaa5063d60c182df21423f33c25
Granite Bagas <granite.sidjabat@sociolla.com>
2025-01-05 22:17:37 +0700

    fix: Fix header logo in email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'GBPS')
-            <img src="https://s3.ap-southeast-1.amazonaws.com/portfolio-granitebps.com/galeries/1600601933_gbps.png"
+            <img src="https://is3.cloudhost.id/gbps/misc/gbps.png"
                 class="logo" alt="GBPS Logo">
             @else
             {{ $slot }}
